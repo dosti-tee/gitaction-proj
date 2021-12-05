5a45cc9dc1463cffca086c3baa5613d5510c5b17
dosti-tee <temiloluwa26@gmail.com>
2021-12-05 20:47:28 +0100

    commented out creds

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket      = "tf-state-adeolutee"
     prefix      = "state"
-    credentials = "credentials.json"
+    # credentials = "credentials.json"
   }
 }
