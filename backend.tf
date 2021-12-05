4fa15dc5413672ad8f9d2e92eecd8a6e59359887
dosti-tee <temiloluwa26@gmail.com>
2021-12-05 21:22:29 +0100

    still cloudn't use a credential.json file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket      = "tf-state-adeolutee"
-    prefix      = "state"
-    credentials = file("${path.module}/credentials.json")
+    bucket = "tf-state-adeolutee"
+    prefix = "state"
   }
 }
--- providers.tf
+++ providers.tf
@@ -1,7 +1,6 @@
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file("${path.module}/credentials.json")
+  project = var.project_id
+  region  = var.region
 }
 
 # provider "random" {
